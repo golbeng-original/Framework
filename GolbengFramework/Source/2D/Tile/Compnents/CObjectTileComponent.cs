c2b057ef4e633d444a688527271b266a463eefa5
GolbengFramework <golbeng0@gmail.com>
2021-01-01 22:55:19 +0900

    CObjectTileComponent.cs 추가

---
+++ GolbengFramework/Source/2D/Tile/Compnents/CObjectTileComponent.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Golbeng.Framework._2D.Tile.Components
+{
+	public class CObjectTileComponent : MonoBehaviour
+	{
+		private CTileConfigure _tileConfigure = CTileAgent.TileConfigure;
+
+		public Vector2Int TileSize;
+
+		public virtual int TileType { get; } = -1;
+
+		protected virtual void Awake() {}
+
+		protected virtual void Start()
+		{
+			AdjustPosition(transform.position);
+		}
+		protected virtual void Update()
+		{
+
+		}
+		protected void AdjustPosition(Vector3 position)
+		{
+			transform.position = _tileConfigure.NormalizeTilePosition(position, TileSize);
+		}
+
+	}
+}
